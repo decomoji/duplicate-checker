3198c8262429c7c45b1e601a42232f08433ae33a
oti <otiext@gmail.com>
2020-09-13 12:11:32 +0900

    feat(main): コマンド引数がない時はデフォルトパスを引き当てる変数を追加した

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const default_path = {
   csv: "./src/candidate.csv",
   all: "./node_modules/decomoji/configs/v5_all.json",
 };
+
+const CSV_PATH = process.argv[2] || default_path.csv;
+const ALL_PATH = process.argv[3] || default_path.all;
+
